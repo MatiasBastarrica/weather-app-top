832c9d875193eb37a595b9f8364bf079467474b4
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-16 20:33:20 -0300

    Store a query example

--- src/index.js
+++ src/index.js
@@ -34,3 +34,7 @@ const moonPhaseNumbers = {
   "last quarter": "0.75",
   "waning crescent": "0.75-1",
 };
+
+let queryExample =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/san%20martin%20buenos%20aires%20argentina/today/?key=HTN87XAAB62SUSSVJXS8JVM33&unitGroup=metric&lang=en&include=days&iconSet=icons2&options=nonulls" +
+  "&elements=conditions,description,datetime,feelslike,icon,precip,precipprob,temp,tempmax,tempmin,uvindex,visibility,winddir,windspeed,moonphase,sunrise,sunset,moonrise,moonset";
