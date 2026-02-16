a022cccaaa4f1825e34569692dced11393f088ec
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-16 20:27:20 -0300

    Store the number ranges of the moonphase

--- src/index.js
+++ src/index.js
@@ -23,3 +23,14 @@ const elements = [
   moonrise,
   moonset,
 ];
+
+const moonPhaseNumbers = {
+  "new moon": "0",
+  "waxing crescent": "0-0.25",
+  "first quarter": "0.25",
+  "waxing gibbous": "0.25-0.5",
+  "full moon": "0.5",
+  "waning gibbous": "0.5-0.75",
+  "last quarter": "0.75",
+  "waning crescent": "0.75-1",
+};
