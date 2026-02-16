17ba84612b0fb049de4dc035f6c41179e1f5b1a8
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-16 20:20:51 -0300

    Write the data elements that will be used in the app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./styles.css";
 
 console.log("Hello, Odinite!!!");
+
+const elements = [
+  conditions,
+  description,
+  datetime,
+  feelslike,
+  icon,
+  precip,
+  precipprob,
+  temp,
+  tempmax,
+  tempmin,
+  uvindex,
+  visibility,
+  winddir,
+  windspeed,
+  moonphase,
+  sunrise,
+  sunset,
+  moonrise,
+  moonset,
+];
