c89ca60f5df88c2ed9523a039a20346be7a2b24e
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-17 09:24:08 -0300

    Fetch the weather data from a location

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import "./styles.css";
 console.log("Hello, Odinite!!!");
 
 const elements = [
-  conditions,
-  description,
-  datetime,
-  feelslike,
-  icon,
-  precip,
-  precipprob,
-  temp,
-  tempmax,
-  tempmin,
-  uvindex,
-  visibility,
-  winddir,
-  windspeed,
-  moonphase,
-  sunrise,
-  sunset,
-  moonrise,
-  moonset,
+  "conditions",
+  "description",
+  "datetime",
+  "feelslike",
+  "icon",
+  "precip",
+  "precipprob",
+  "temp",
+  "tempmax",
+  "tempmin",
+  "uvindex",
+  "visibility",
+  "winddir",
+  "windspeed",
+  "moonphase",
+  "sunrise",
+  "sunset",
+  "moonrise",
+  "moonset",
 ];
 
 const moonPhaseNumbers = {
@@ -38,3 +38,20 @@ const moonPhaseNumbers = {
 let queryExample =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/san%20martin%20buenos%20aires%20argentina/today/?key=HTN87XAAB62SUSSVJXS8JVM33&unitGroup=metric&lang=en&include=days&iconSet=icons2&options=nonulls" +
   "&elements=conditions,description,datetime,feelslike,icon,precip,precipprob,temp,tempmax,tempmin,uvindex,visibility,winddir,windspeed,moonphase,sunrise,sunset,moonrise,moonset";
+
+let weatherData;
+
+async function getWeather() {
+  await fetch(queryExample)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      weatherData = data.days[0];
+      console.log(data);
+    });
+
+  console.log(weatherData);
+}
+
+getWeather();
